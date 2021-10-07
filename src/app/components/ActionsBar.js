b38f16f4932d013870ec23a5ad859a6960154359
Italo Fontes <italo5123@hotmail.com>
2021-10-07 17:08:23 -0300

    :sparkles: test

---
+++ src/app/components/ActionsBar.js
@@ -0,0 +1,6 @@
+import {html} from "lit"
+import CustomElement from "../core/CustomElement";
+
+export default class ActionsBar extends CustomElement {
+  
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,3 @@
 import AppController from "./components/AppController";
-import Button from "./components/Button";
 
 customElements.define("app-controller", AppController);
-customElements.define("custom-button", Button);
